24adfa1b790fc6f2cf9bb9d71fa40132a0e9ccab
Pukhraj88 <136183209+Pukhraj88@users.noreply.github.com>
2026-05-29 09:09:04 +0530

    Update PreviewButton.tsx

--- src/components/InputElements/PreviewButton.tsx
+++ src/components/InputElements/PreviewButton.tsx
@@ -1,12 +1,11 @@
 import { Button, Col, Divider, Form, Modal, Row } from "antd";
-import { clearPreviewData } from "next/dist/server/api-utils";
 import { memo, useState } from "react";
 import { Typography } from "antd";
 
 const { Title, Text } = Typography;
 
 const PreviewButton = memo((props: any) => {
-  const { form, name, label } = props;
+  const { form} = props;
 
 type PreviewData={
    name?: string;
@@ -15,10 +14,10 @@ type PreviewData={
   dob?: any; 
   occupation?: string;
   education?: string;
-  f_name?: string;
-  f_occupation?: string;
-  m_name?: string;
-  m_occupation?: string;
+  father_name?: string;
+  father_occupation?: string;
+  mother_name?: string;
+  mother_occupation?: string;
   sibling_name?: string;
   sibling_education?: string;
   sibling_occupation?: string;
@@ -113,25 +112,25 @@ type PreviewData={
     <Col span={12}>
       <Text strong>Father's Name:</Text>
       <br />
-      <Text>{dataPreview.f_name || "Not provided"}</Text>
+      <Text>{dataPreview.father_name || "Not provided"}</Text>
     </Col>
 
       <Col span={12}>
       <Text strong>Father's Occupation:</Text>
       <br />
-      <Text>{dataPreview.f_occupation || "Not provided"}</Text>
+      <Text>{dataPreview.father_occupation || "Not provided"}</Text>
     </Col>
 
     <Col span={12}>
       <Text strong>Mother's Name:</Text>
       <br />
-      <Text>{dataPreview.m_name || "Not provided"}</Text>
+      <Text>{dataPreview.mother_name || "Not provided"}</Text>
     </Col>
 
     <Col span={12}>
       <Text strong>Mother's Occupation:</Text>
       <br />
-      <Text>{dataPreview.m_occupation || "Not provided"}</Text>
+      <Text>{dataPreview.mother_occupation || "Not provided"}</Text>
     </Col>
 
       <Col span={12}>
@@ -173,83 +172,3 @@ export default PreviewButton;
 
 
 
-
-
-// import { Button, Form, Modal } from "antd";
-// import { clearPreviewData } from "next/dist/server/api-utils";
-// import { memo, useState } from "react";
-
-// const PreviewButton = memo((props: any) => {
-//   const { form, name, label } = props;
-
-// type PreviewData={
-//    name?: string;
-//   gender?: { value?: string };
-//   relation?: { value?: string };
-//   dob?: any; 
-//   occupation?: string;
-//   education?: string;
-//   f_name?: string;
-//   f_occupation?: string;
-//   m_name?: string;
-//   m_occupation?: string;
-//   sibling_name?: string;
-//   sibling_education?: string;
-//   sibling_occupation?: string;
-// }
-
-//   const [isPreviewOpen, setisPreviewOpen] = useState<boolean>(false);
-//   const [dataPreview, setDataPreview] = useState<PreviewData>({});
-
-//   const handlePreviewButton = () => {
-//     const previewData = form.getFieldsValue();
-//     setDataPreview(previewData);
-//     setisPreviewOpen(true);
-//   };
-
-//   const hanldeClosePreview = () => {
-//     setisPreviewOpen(false);
-//   };
-
-//   return (
-//     <div>
-//       <Modal
-//         open={isPreviewOpen}
-//         onCancel={hanldeClosePreview}
-//         onOk={hanldeClosePreview}
-//       >
-//         <h1>Review Everything</h1>
-//         <p>Name: {dataPreview.name || "Not provided"}</p>
-//         <p>Gender: {dataPreview.gender?.value || "Not provided"}</p>
-//         <p>Relation: {dataPreview.relation?.value || "Not provided"}</p>
-//         <p>
-//           Date of Birth:
-//           {`${dataPreview.dob?.day}/${dataPreview.dob?.month}/${dataPreview.dob?.year}`}
-//         </p>
-//         <p>Occupation: {dataPreview.occupation || "Not provided"}</p>
-//         <p>Education: {dataPreview.education || "Not provided"}</p>
-//         <p>Father's Name: {dataPreview.f_name || "Not provided"}</p>
-//         <p>Father's Occupation: {dataPreview.f_occupation || "Not provided"}</p>
-//         <p>Mother's Name: {dataPreview.m_name || "Not provided"}</p>
-//         <p>Mother's Occupation: {dataPreview.m_occupation || "Not provided"}</p>
-//         <p>Sibling Name: {dataPreview.sibling_name || "Not provided"}</p>
-//         <p>
-//           Sibling Education: {dataPreview.sibling_education || "Not provided"}
-//         </p>
-//         <p>
-//           Sibling Occupation:{" "}
-//           {dataPreview.sibling_occupation || "Not provided"}{" "}
-//         </p>
-//       </Modal>
-
-//       <Form.Item>
-//         <Button type="primary" onClick={handlePreviewButton}>
-//           Preview
-//         </Button>
-//       </Form.Item>
-//     </div>
-//   );
-// });
-
-// PreviewButton.displayName = "PreviewButton";
-// export default PreviewButton;

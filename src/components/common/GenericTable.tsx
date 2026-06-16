968b241267b84f2d0762ae5c410eb5a12ce2ce48
unknown <pukhrajnemiwal9@gmail.com>
2026-06-16 10:10:42 +0530

    gotra occupation serach field updated

--- src/components/common/GenericTable.tsx
+++ src/components/common/GenericTable.tsx
@@ -1,53 +1,97 @@
-import { Button, Col, Row, Select, Space, Table } from "antd";
+import { Button, Col, Form, Row, Select, Space, Table } from "antd";
 import Text from "antd/es/typography/Text";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import InputField from "../InputElements/InputField";
+import debounce from "lodash/debounce";
 
 interface GenericTableProps {
   data: any[];
   loading: boolean;
   columns: any;
   pagination: any;
-  handleSearch: any;
-  handleSort: any;
+  handleSearch: (value: string) => void;
+  handleSort: (sorter: any) => void;
+  setSearch: (value: string) => void;
 }
 
 export const GenericTable = (props: GenericTableProps) => {
-  const { data, loading, columns, pagination, handleSearch,handleSort } = props;
+  const {
+    data,
+    loading,
+    columns,
+    pagination,
+    handleSearch,
+    handleSort,
+    setSearch,
+  } = props;
+  const [inputValue, setInputValue] = useState<string>("");
 
-  const [inputValue, setInputValue] = useState("");
+  // debouncing funtion
+  const debounceSearch = useMemo(
+    () =>
+      debounce((value: string) => {
+        setSearch(value);
+      }, 500),
+    [setSearch],
+  );
 
- 
+  // handle Onchnage State
+  const handleInputChange = (v: string) => {
+    setInputValue(v);
+    if (!v) {
+      debounceSearch("");
+      return;
+    }
+
+    if (v.length < 3) {
+      return;
+    }
+    debounceSearch(v);
+  };
 
   return (
     <>
-      <Space direction="vertical" size={4} style={{ marginBottom: "1rem" }}>
-        <Text strong>Search Gotra</Text>
-        <Row>
-          <Col>
-           <InputField
-           style={{height:"1.5rem"}}
-          name="Search"
-          label={null}
-          onChange={(v) => setInputValue(v.target.value)}
-        />
-          </Col>
-          <Col>
-        <Button type="primary" onClick={() => handleSearch(inputValue)} style={{height:"1.5rem" ,margin:".2rem"}}>Go</Button>
-          
-          </Col>
-        </Row>
-       
-      </Space>
-
-
-
-      
+      <Form>
+        <Space direction="vertical" size={4} style={{ marginBottom: "1rem" }}>
+          <Text strong>Search Gotra</Text>
+          <Row>
+            <Col>
+              <InputField
+                style={{ height: "1.5rem" }}
+                name="Search"
+                label={null}
+                onChange={(v) => handleInputChange(v.target.value)}
+                rules={[
+                  {
+                    validator: (_: any, value: string) => {
+                      if (!value || value.length >= 3) {
+                        return Promise.resolve();
+                      }
+                      return Promise.reject(
+                        new Error("Minimum 3 Character required"),
+                      );
+                    },
+                  },
+                ]}
+              />
+            </Col>
+            <Col>
+              <Button
+                type="primary"
+                onClick={() => handleSearch(inputValue)}
+                style={{ height: "1.5rem", margin: ".2rem" }}
+              >
+                Go
+              </Button>
+            </Col>
+          </Row>
+        </Space>
+      </Form>
 
       <Table
-      onChange={(pagination, filters, sorter) => {
-        handleSort(sorter)
-  }}
+        onChange={(pagination, filters, sorter) => {
+          handleSort(sorter);
+        }}
         rowKey="id"
         bordered
         size="small"

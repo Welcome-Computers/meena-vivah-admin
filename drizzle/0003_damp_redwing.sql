7f228865b10a68dd7b94d70bd3bd94644be41227
ujagarsingh <ujagarsinghmeena@nattyhatty.com>
2026-06-26 10:06:01 +0530

    Track Drizzle migrations

---
+++ drizzle/0003_damp_redwing.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `imported_profile` MODIFY COLUMN `name` varchar(150);--> statement-breakpoint
+ALTER TABLE `imported_profile` MODIFY COLUMN `fathersname` varchar(150);--> statement-breakpoint
+ALTER TABLE `imported_profile` MODIFY COLUMN `self_gotra` varchar(50);--> statement-breakpoint
+ALTER TABLE `imported_profile` MODIFY COLUMN `m_gotra` varchar(50);--> statement-breakpoint
+ALTER TABLE `imported_profile` MODIFY COLUMN `gm_gotra` varchar(50);--> statement-breakpoint
+ALTER TABLE `imported_profile` MODIFY COLUMN `mat_gm_gotra` varchar(50);
